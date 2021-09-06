57ea8a37280bcd6bd44446b5e390beec011fd023
asadanik <s.engineer63@gmail.com>
2021-09-06 12:58:01 +0600

    Setup done with communication client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
+
+    React.useEffect(() => {
+        axios.get("/api/getBook?id=612a091f03c12607c489f27c")
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => console.log(err.message));
+    }, []);
+
+    console.log('Hello world');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+        <h1>Hello React | Node World!</h1>
+      </React.Fragment>
   );
 }
 
