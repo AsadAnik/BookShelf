1336cb1f57ef81b20e0a9a963dcd73aee64756fd
AsadAnik <s.engineer63@gmail.com>
2022-05-01 11:00:13 +0600

    database updated

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
 const config = {
     production: {
         SECRET: process.env.SECRET,
+        // mongodb+srv://asadanik:asadanik170984@bookshelf.nrsmw.mongodb.net/bookShelf?retryWrites=true&w=majority
         DATABASE: process.env.MONGODB_URI
     },
     default: {
